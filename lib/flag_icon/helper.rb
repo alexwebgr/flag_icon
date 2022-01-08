2c41e21df53569019e88f38ff919ca1b919b3ea3
alexwebgr <websurfer992@gmail.com>
2022-01-08 18:21:17 +0200

    made available and popular configurable

--- lib/flag_icon/helper.rb
+++ lib/flag_icon/helper.rb
@@ -1,6 +1,8 @@
 require "flag_icon/countries"
 
 module FlagIcon
+  private
+
   def language_name(code)
     FlagIcon::Countries::LANGUAGES[code.to_sym]
   end
@@ -9,18 +11,24 @@ module FlagIcon
     FlagIcon::Countries::LANGUAGE_FLAGS[code.to_sym]
   end
 
-  def languages
-    @languages ||= FlagIcon::Countries::LANGUAGES.keys
-  end
-
   def select_language
     {
-      'Popular' => [['English', :en], ['Spanish', :es]],
-      'Available' => languages.map { |lang| [language_name(lang), lang] }
+      'Popular' => popular_languages,
+      'Available' => available_languages.map { |lang| [language_name(lang), lang] }
     }
   end
 
   def flag_icon(code)
-    "<span class='flag-icon flag-icon-#{language_flag(code)}' title='Audio language - #{language_name code}'></span>"
+    "<span class='flag-icon flag-icon-#{language_flag(code)}' title='Audio language - #{language_name(code)}'></span>"
+  end
+
+  public
+
+  def popular_languages
+    [['English', :en], ['Spanish', :es]]
+  end
+
+  def available_languages
+    @languages ||= FlagIcon::Countries::LANGUAGES.keys
   end
 end
