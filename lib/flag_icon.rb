c10cbde96dab046dad157b0dfe297f1aea977a06
alexwebgr <websurfer992@gmail.com>
2022-01-09 08:25:43 +0200

    updated helper

--- lib/flag_icon.rb
+++ lib/flag_icon.rb
@@ -2,3 +2,6 @@ require "flag_icon/helper"
 require "flag_icon/countries"
 require "flag_icon/railtie"
 require 'flag_icon/rails'
+
+module FlagIcon
+end
--- lib/flag_icon/helper.rb
+++ lib/flag_icon/helper.rb
@@ -1,40 +1,38 @@
 module FlagIcon
-  private
+  class << self
+    def language_name(code)
+      FlagIcon::Countries::LANGUAGES[code.to_sym]
+    end
 
-  def language_name(code)
-    FlagIcon::Countries::LANGUAGES[code.to_sym]
-  end
+    def country_name(code)
+      FlagIcon::Countries::NAMES[code.to_sym]
+    end
 
-  def country_name(code)
-    FlagIcon::Countries::NAMES[code.to_sym]
-  end
+    def language_flag(code)
+      FlagIcon::Countries::LANGUAGE_FLAGS[code.to_sym]
+    end
 
-  def language_flag(code)
-    FlagIcon::Countries::LANGUAGE_FLAGS[code.to_sym]
-  end
+    def select_language
+      {
+        'Popular' => popular_languages,
+        'Available' => available_languages.map { |lang| [language_name(lang), lang] }
+      }
+    end
 
-  def select_language
-    {
-      'Popular' => popular_languages,
-      'Available' => available_languages.map { |lang| [language_name(lang), lang] }
-    }
-  end
+    def language_icon(code)
+      "<span class='flag-icon flag-icon-#{language_flag(code)}' title='Audio language - #{language_name(code)}'></span>"
+    end
 
-  def language_icon(code)
-    "<span class='flag-icon flag-icon-#{language_flag(code)}' title='Audio language - #{language_name(code)}'></span>"
-  end
+    def flag_icon(code)
+      "<span class='flag-icon flag-icon-#{code}' title='Available in #{country_name(code)}'></span>"
+    end
 
-  def flag_icon(code)
-    "<span class='flag-icon flag-icon-#{code}' title='Available in #{country_name(code)}'></span>"
-  end
+    def popular_languages
+      [['English', :en], ['Spanish', :es]]
+    end
 
-  public
-
-  def popular_languages
-    [['English', :en], ['Spanish', :es]]
-  end
-
-  def available_languages
-    @languages ||= FlagIcon::Countries::LANGUAGES.keys
+    def available_languages
+      @languages ||= FlagIcon::Countries::LANGUAGES.keys
+    end
   end
 end
